091b45ae925cb958d55ffbfd06fa22955796b47f
weizman <weizmangal@gmail.com>
2023-01-22 23:25:51 +0200

    change api to class

--- README.md
+++ README.md
@@ -13,21 +13,37 @@ check out the [demo](https://lavamoat.github.io/walker/demo/)
 ## use
 
 ```javascript
-const walk = require('@lavamoat/walker');
+const Walker = require('@lavamoat/walker');
 
-walk(start, cb, opts = {}); // example
+new Walker(cb, opts = {}).walk(start); // example
 
-walk(window, (value, path) => {
+new Walker((value, path) => {
     console.log('found value:', value);
     console.log('path to value was:', path);
     if (value === 1) {
         return true; // true means stop walker
     }
-});
+}, {maxRecursionLimit: 9}).walk(window);
 
-// options object can be passed as third argument optionally:
+// options object can be passed as second argument to Walker constructor optionally:
 const opts = {
-    limit: 3, // recursive depth
-    key: (k) => '__' + k + '__', // customize keys that construct the path
+    // a function with which the visited keys during walking 
+    // process can be customizd for how they appear within 
+    // @path argument (useful for aggregation purposes).
+    generateKey, // (k) => '__' + k + '__'
+    // a boolean to indicate whether to use or avoid cache for visited values.
+    // when true, walker will not skip walking into non primitive values that were already processed.
+    avoidValuesCache, // true/false [default false]
+    // a Set when prefered providing your own cache Set when @avoidValuesCache is set to false.
+    // useful when running multiple walkers in a single execution.
+    valuesCacheSet, // new Set()
+    // a boolean to indicate whether to use or avoid cache for visited properties.
+    // when true, walker will not avoid generating own properties of objects to walk into more than once.
+    avoidPropertiesCache, // true/false [default false]
+    // a Map when prefered providing your own cache Map when @avoidPropertiesCache is set to false.
+    // useful when running multiple walkers in a single execution.
+    propertiesCacheMap, // new Map()
+    // a number to indicate the maximum recursion depth walker is allowed to walk.
+    maxRecursionLimit, // 3 [default 5]
 };
 ```
---
+++ src/walker.js
@@ -0,0 +1,96 @@
+const shouldIgnore = require('./ignores');
+const getAllProps = require('./properties');
+
+function isPrimitive(obj) {
+    if (obj === null) {
+        return true;
+    }
+    const type = typeof obj;
+    return (
+        type === 'bigint' ||
+        type === 'boolean' ||
+        type === 'number' ||
+        type === 'string'
+    );
+
+}
+
+Walker.prototype.shouldWalk = function(obj, limit) {
+    if (isPrimitive(obj)) {
+        return false;
+    }
+    if (limit === 0) {
+        return false;
+    }
+    if (this.avoidValuesCache) {
+        return true;
+    }
+    if (this.valuesCacheSet.has(obj)) {
+        return false;
+    }
+    this.valuesCacheSet.add(obj);
+    return true;
+}
+
+Walker.prototype.walkRecursively = function(obj, limit, keys = [], values = []) {
+    if (!this.shouldWalk(obj, limit)) {
+        return false;
+    }
+    const cache = !this.avoidPropertiesCache && this.propertiesCacheMap;
+    const props = getAllProps(obj, cache);
+    for (let i = 0; i < props.length; i++) {
+        const prop = props[i];
+        if (shouldIgnore(prop, obj, values)) {
+            continue;
+        }
+        const val = obj[prop];
+        const newKeys = [...keys, this.generateKey(prop, val)];
+        const newValues = [...values, obj];
+        if (this.walkRecursively(val, limit - 1, newKeys, newValues) || this.cb(val, newKeys)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function Walker(cb, {
+                    generateKey,
+                    avoidValuesCache,
+                    avoidPropertiesCache,
+                    valuesCacheSet,
+                    propertiesCacheMap,
+                    maxRecursionLimit,
+                }) {
+    if (typeof cb !== 'function') {
+        throw new Error(`@cb must be a function, instead got a "${typeof cb}"`);
+    }
+    if (typeof generateKey !== 'function') {
+        generateKey = (prop, val) => `${({}).toString.call(val)}:${prop}`;
+    }
+    if (typeof maxRecursionLimit !== 'number') {
+        maxRecursionLimit = 5;
+    }
+    if (!(avoidPropertiesCache = Boolean(avoidPropertiesCache))) {
+        if (typeof propertiesCacheMap !== 'object') {
+            propertiesCacheMap = new Map();
+        }
+    }
+    if (!(avoidValuesCache = Boolean(avoidValuesCache))) {
+        if (typeof valuesCacheSet !== 'object') {
+            valuesCacheSet = new Set();
+        }
+    }
+    this.cb = cb;
+    this.generateKey = generateKey;
+    this.avoidValuesCache = avoidValuesCache;
+    this.avoidPropertiesCache = avoidPropertiesCache;
+    this.valuesCacheSet = valuesCacheSet;
+    this.propertiesCacheMap = propertiesCacheMap;
+    this.maxRecursionLimit = maxRecursionLimit;
+}
+
+Walker.prototype.walk = function(start) {
+    return this.walkRecursively(start, this.maxRecursionLimit);
+}
+
+module.exports = Walker;
